6289a2e2cb8a6034f6fd2c7dbf1e7fa1ab4a297f
Aleksey Trotsiuk <aleksey.trotsiuk@gmail.com>
2016-07-05 14:42:59 +0300

    close all dropdowns when the new one opens

--- components/select/select.ts
+++ components/select/select.ts
@@ -339,9 +339,11 @@ export class SelectComponent implements OnInit {
     }
   }
 
-  public clickedOutside():void  {
-    this.inputMode = false;
-    this.optionsOpened = false;
+  public clickedOutside(event: any):void  {
+    if (!this.element.nativeElement.contains(event.data)) {
+      this.inputMode = false;
+      this.optionsOpened = false;
+    }
   }
 
   public get firstItemHasChildren():boolean {
